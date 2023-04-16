58b89511d74a18d00894666d5d49447b4a19afab
kevinolweb <kevin.oleary91@gmail.com>
2023-04-16 16:51:07 +0000

    Fixes

--- cuforum/settings.py
+++ cuforum/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["cuforum-v2.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["cuforumv2.herokuapp.com", "localhost"]
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
