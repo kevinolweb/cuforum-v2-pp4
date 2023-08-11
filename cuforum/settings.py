9b6b1d7e7a920ab86560d1c2b39c45aade13231b
kevinolweb <kevin.oleary91@gmail.com>
2023-08-11 13:14:42 +0000

    settings set up for production and debug false

--- cuforum/settings.py
+++ cuforum/settings.py
@@ -14,12 +14,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'g*2m-tli@m8v#$fvc%8lo86%5ooh6y&#45w=3*imshmhf*_b%*')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 #DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
-#ALLOWED_HOSTS = ['cuforumv2.herokuapp.com', 'localhost']
-ALLOWED_HOSTS = ['8000-kevinolweb-cuforumv2pp4-jo1kfnsc97z.ws-eu102.gitpod.io', 'localhost']
+DEBUG = False
+ALLOWED_HOSTS = ['cuforumv2.herokuapp.com', 'localhost']
+#ALLOWED_HOSTS = ['8000-kevinolweb-cuforumv2pp4-jo1kfnsc97z.ws-eu102.gitpod.io', 'localhost']
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
